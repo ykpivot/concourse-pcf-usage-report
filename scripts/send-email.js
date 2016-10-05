57a44933dc61955c136c0b95f4f065ca66d30702
Luciano Silva <lsilva@pivotal.io>
2016-10-05 18:48:42 -0400

    Fix email job

--- scripts/send-email.js
+++ scripts/send-email.js
@@ -4,11 +4,21 @@ var nodemailer = require("nodemailer");
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport('smtps://'+process.env.SMTP_USERNAME+':'+process.env.SMTP_PASSWORD+'@'+process.env.SMTP_HOST+':'+process.env.SMTP_PORT);
 
-var emailSubjectFile=process.env.EMAIL_SUBJECT_FILE;
-var emailTextFile=process.env.EMAIL_TEXT_FILE;
+var emailSubject="";
+if (process.env.EMAIL_SUBJECT_TEXT) {
+  emailSubject=process.env.EMAIL_SUBJECT_TEXT;
+} else {
+  var emailSubjectFile=process.env.EMAIL_SUBJECT_FILE;
+  emailSubject=fs.readFileSync(emailSubjectFile, 'utf8');
+}
 
-var emailSubject=fs.readFileSync(emailSubjectFile, 'utf8');
-var emailText=fs.readFileSync(emailTextFile, 'utf8');
+var emailText="";
+if (process.env.EMAIL_BODY_TEXT) {
+  emailText=process.env.EMAIL_BODY_TEXT;
+} else {
+  var emailTextFile=process.env.EMAIL_BODY_FILE;
+  emailText=fs.readFileSync(emailTextFile, 'utf8');
+}
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
