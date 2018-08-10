b6c05b4910a87104a149d1cc3fc0878ec8182a6b
Yosep Kim <yosepk@gmail.com>
2018-08-10 10:56:12 -0400

    prints out debug messages

--- scripts/GetPCFUsageData.js
+++ scripts/GetPCFUsageData.js
@@ -18,7 +18,7 @@ GetPCFUsageData.prototype.execute = function() {
 
 GetPCFUsageData.prototype.cfLogin = function() {
   var cmd_login = 'cf api '+process.env.PCF_API_ENDPOINT+' --skip-ssl-validation && cf login -u '+process.env.SYS_ADMIN_USER+' -p '+process.env.SYS_ADMIN_PASSWORD+' -o '+process.env.PCF_ORG+' -s '+process.env.PCF_SPACE;
-  console.log("Issuing cf api and login commands");
+  console.log("Issuing cf api and login commands: " + cmd_login);
   var currentGetPCFUsageDataObject = this;
   exec(cmd_login, function(error, stdout, stderr) {
     if (! currentGetPCFUsageDataObject.execError("cfLogin",error,stderr)) {
